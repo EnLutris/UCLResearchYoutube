94d5eb42f95aa101e87ee7db2582bcc136b14534
ESRA <esraogz@gmail.com>
2023-03-03 09:22:49 +0100

    second commit

--- app.py
+++ app.py
@@ -10,11 +10,17 @@ import re
 port = os.environ.get('PORT', 8000)
 app = FastAPI(PORT = port)
 
+class Item(BaseModel):
+    url: str
+
 
 @app.get("/")
 async def status_check():
   return "alive"
 
+@app.get("/comments")
+async def data_format():
+  return ("url should be submitted in quotes")
 
 @app.post("/uploadfile/")
 async def process(file: UploadFile = File(...)):
@@ -29,3 +35,11 @@ async def process(file: UploadFile = File(...)):
            comments = feriha(url)
            comment_list.append(comments)
         return comment_list
+          
+    
+
+@app.post("/comments")
+async def youtube_comments(url: str):
+  print(url)
+  comments = feriha(url)
+  return comments
